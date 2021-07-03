846a2e3c63195856ee9414d98b7f70db16fd1d43
blowhite <blowhite2416@naver.com>
2021-07-03 11:26:30 +0900

    2021-07-03 before jump

--- components/PostCard.js
+++ components/PostCard.js
@@ -5,7 +5,7 @@ import { RetweetOutlined, HeartOutlined, MessageOutlined, EllipsisOutlined, Hear
 import { useSelector } from 'react-redux';
 import PostImages from './PostImages';
 import CommentForm from './CommentForm';
-
+import PostCardContent from './PostCardContent';
 
 const PostCard = ({ post }) => {
      const [liked, setLiked] = useState(false);
@@ -46,7 +46,7 @@ const PostCard = ({ post }) => {
                <Card.Meta
                  avatar={<Avatar>{post.user.nickname[0]}</Avatar>}
                  title={post.user.nickname}
-                 description={post.content}
+                 description={<PostCardContent postData={post.content} />}
                  />
                  
                {/* <Button> */}
